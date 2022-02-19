428979c2df8e451ea437ad09f75d8958e6579252
kbrac <kktkate28@gmail.com>
2022-02-20 00:47:08 +0800

    update2

--- lib/screenv2.dart
+++ lib/screenv2.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Screenv2 extends StatefulWidget {
   const Screenv2({Key? key}) : super(key: key);
@@ -12,7 +13,6 @@ class Screenv2 extends StatefulWidget {
 }
 
 class _Screenv2State extends State<Screenv2> {
-
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
   // List<int> numbers = [8, 2, 9, 4, 5, 10, 7, 1, 3, 6, 0, 12, 11, 14, 15, 13];
   bool isEmpty = true;
@@ -36,6 +36,21 @@ class _Screenv2State extends State<Screenv2> {
     14: [10, 13, 15],
     15: [11, 14],
   };
+
+  List<MaterialColor> colorizeColors = [
+    Colors.purple,
+    Colors.blue,
+    Colors.yellow,
+    Colors.red,
+  ];
+
+  TextStyle colorizeTextStyle = TextStyle(
+    fontSize: 40.0,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Cabin',
+  );
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +81,17 @@ class _Screenv2State extends State<Screenv2> {
                     bottomRight: Radius.circular(60),
                     topLeft: Radius.circular(10),
                     topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
                   ),
+                  boxShadow: [
+                    BoxShadow(
+                      blurStyle: BlurStyle.inner,
+                      color: Colors.teal,
+                      spreadRadius: 0.5,
+                      blurRadius: 5,
+                      offset: Offset(7, 15),
+                    ),
+                  ],
                   color: Color(0xFF57bcb2),
                 ),
                 child: Column(
@@ -94,10 +119,11 @@ class _Screenv2State extends State<Screenv2> {
                           ),
                           color: Colors.black,
                         ),
-                        child:  Row(
+                        child: Row(
                           children: [
                             Padding(
-                              padding: const EdgeInsets.only(left:10, bottom: 70),
+                              padding:
+                                  const EdgeInsets.only(left: 10, bottom: 70),
                               child: Container(
                                 height: 5,
                                 width: 5,
@@ -109,7 +135,8 @@ class _Screenv2State extends State<Screenv2> {
                                       color: Colors.red.withOpacity(0.6),
                                       spreadRadius: 7,
                                       blurRadius: 7,
-                                      offset: Offset(0,3), // changes position of shadow
+                                      offset: Offset(
+                                          0, 3), // changes position of shadow
                                     ),
                                   ],
                                 ),
@@ -117,7 +144,8 @@ class _Screenv2State extends State<Screenv2> {
                             ),
                             Expanded(
                               child: Padding(
-                                padding: const EdgeInsets.only(left: 20, right: 30, top: 20, bottom: 20),
+                                padding: const EdgeInsets.only(
+                                    left: 20, right: 30, top: 20, bottom: 20),
                                 child: Container(
                                   decoration: const BoxDecoration(
                                     borderRadius: BorderRadius.only(
@@ -126,50 +154,81 @@ class _Screenv2State extends State<Screenv2> {
                                       topRight: Radius.circular(10),
                                       bottomLeft: Radius.circular(10),
                                     ),
-                                    color: Colors.amberAccent,
+                                    color: Colors.white,
                                   ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 5),
-                                    child: GridView.builder(
-                                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: 4,
-                                        mainAxisSpacing: 5,
-                                        crossAxisSpacing: 5,
-                                        // childAspectRatio: 1.4,
-                                      ),
-                                      itemCount: numbers.length,
-                                      itemBuilder: (BuildContext context, int index) {
-                                        if (numbers[index] != 0) {
-                                          return GestureDetector(
-                                            onTap: () {
-                                              for (int i = 0; i < results[index]!.length; i++) {
-                                                int index2 = results[index]![i];
-                                                if (numbers[index2] == 0) {
-                                                  setState(() {
-                                                    numbers[index2] = numbers[index];
-                                                    numbers[index] = 0;
-                                                    move++;
-                                                  });
-                                                }
-                                              }
-                                            },
-                                            child: Container(
-                                              decoration: BoxDecoration(
-                                                color: Colors.white,
-                                                borderRadius:
-                                                BorderRadius.all(Radius.circular(10)),
-                                                image: DecorationImage(
-                                                  image: AssetImage('images/dash_' + numbers[index].toString() + '.jpg'),
-                                                  fit: BoxFit.fill,
+                                  child:
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      //TODO:FixAnimation
+                                      // AnimatedTextKit(
+                                      //   onFinished: () {
+                                      //   },
+                                      //   totalRepeatCount: 1,
+                                      //   animatedTexts: [
+                                      //     ColorizeAnimatedText(
+                                      //       'FLUTTERBOY',
+                                      //       textStyle: colorizeTextStyle,
+                                      //       colors: colorizeColors,
+                                      //       speed: Duration(milliseconds: 300),
+                                      //     ),
+                                      //   ],
+                                      // ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 5, right: 5, top: 10, bottom: 5),
+                                        child: GridView.builder(
+                                          gridDelegate:
+                                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 4,
+                                            mainAxisSpacing: 5,
+                                            crossAxisSpacing: 5,
+                                            // childAspectRatio: 1.4,
+                                          ),
+                                          itemCount: numbers.length,
+                                          itemBuilder:
+                                              (BuildContext context, int index) {
+                                            if (numbers[index] != 0) {
+                                              return GestureDetector(
+                                                onTap: () {
+                                                  for (int i = 0;
+                                                      i < results[index]!.length;
+                                                      i++) {
+                                                    int index2 = results[index]![i];
+                                                    if (numbers[index2] == 0) {
+                                                      setState(() {
+                                                        numbers[index2] =
+                                                            numbers[index];
+                                                        numbers[index] = 0;
+                                                        move++;
+                                                      });
+                                                    }
+                                                  }
+                                                },
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                    color: Colors.white,
+                                                    borderRadius: BorderRadius.all(
+                                                        Radius.circular(10)),
+                                                    image: DecorationImage(
+                                                      image: AssetImage(
+                                                          'images/dash_' +
+                                                              numbers[index]
+                                                                  .toString() +
+                                                              '.jpg'),
+                                                      fit: BoxFit.fill,
+                                                    ),
+                                                  ),
+                                                  child: Text(
+                                                      numbers[index].toString()),
                                                 ),
-                                              ),
-                                              child: Text(numbers[index].toString()),
-                                            ),
-                                          );
-                                        }
-                                        return SizedBox.shrink();
-                                      },
-                                    ),
+                                              );
+                                            }
+                                            return SizedBox.shrink();
+                                          },
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
@@ -182,27 +241,34 @@ class _Screenv2State extends State<Screenv2> {
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Padding(
-                          padding:
-                          const EdgeInsets.only(top: 5, left: 20, right: 30),
+                          padding: const EdgeInsets.only(
+                              top: 5, left: 20, right: 30),
                           child: Container(
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
                               color: Colors.teal.shade200,
+                              boxShadow: [
+                                BoxShadow(
+                                  blurStyle: BlurStyle.inner,
+                                  color: Colors.teal,
+                                  spreadRadius: 0.5,
+                                  blurRadius: 5,
+                                  offset: Offset(6, 10),
+                                )
+                              ],
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(10.0),
-                              child: Transform.scale(
-                                scaleY: 0.95,
-                                child: const FaIcon(
-                                  FontAwesomeIcons.plus,
-                                  color: Colors.black,
-                                  size: 85,
-                                ),
+                              child: const FaIcon(
+                                FontAwesomeIcons.plus,
+                                color: Colors.black,
+                                size: 85,
                               ),
                             ),
                           ),
                         ),
-                        Stack(//TODO:Tilt the buttons a bit more
+                        Stack(
+                          //TODO:Tilt the buttons a bit more
                           children: [
                             Positioned(
                               left: 69,
@@ -214,13 +280,21 @@ class _Screenv2State extends State<Screenv2> {
                                   decoration: BoxDecoration(
                                     shape: BoxShape.rectangle,
                                     color: Colors.teal.shade200,
-                                    borderRadius:
-                                    const BorderRadius.only(
+                                    borderRadius: const BorderRadius.only(
                                       topRight: Radius.circular(50),
                                       topLeft: Radius.circular(30),
                                       bottomLeft: Radius.circular(50),
                                       bottomRight: Radius.circular(30),
                                     ),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        blurStyle: BlurStyle.inner,
+                                        color: Colors.teal,
+                                        spreadRadius: 0.5,
+                                        blurRadius: 5,
+                                        offset: Offset(6, 10),
+                                      )
+                                    ],
                                   ),
                                 ),
                               ),
@@ -240,8 +314,8 @@ class _Screenv2State extends State<Screenv2> {
                                   ),
                                 ),
                                 Padding(
-                                  padding:
-                                  const EdgeInsets.only(left: 35, right: 30, bottom: 5),
+                                  padding: const EdgeInsets.only(
+                                      left: 35, right: 30, bottom: 5),
                                   child: Container(
                                     height: 40,
                                     width: 40,
@@ -261,8 +335,8 @@ class _Screenv2State extends State<Screenv2> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Padding(
-                          padding:
-                          EdgeInsets.only(top: 20, left: 30, right: 30, bottom:10),
+                          padding: EdgeInsets.only(
+                              top: 20, left: 30, right: 30, bottom: 10),
                           child: Container(
                             height: 20,
                             width: 50,
@@ -270,12 +344,21 @@ class _Screenv2State extends State<Screenv2> {
                               shape: BoxShape.rectangle,
                               color: Colors.black,
                               borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurStyle: BlurStyle.inner,
+                                  color: Colors.black26,
+                                  spreadRadius: 0.5,
+                                  blurRadius: 5,
+                                  offset: Offset(3, 6),
+                                ),
+                              ],
                             ),
                           ),
                         ),
                         Padding(
-                          padding:
-                          EdgeInsets.only(top: 20, left: 30, right: 30, bottom:10),
+                          padding: EdgeInsets.only(
+                              top: 20, left: 30, right: 30, bottom: 10),
                           child: Container(
                             height: 20,
                             width: 50,
@@ -283,6 +366,15 @@ class _Screenv2State extends State<Screenv2> {
                               shape: BoxShape.rectangle,
                               color: Colors.black,
                               borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurStyle: BlurStyle.inner,
+                                  color: Colors.black26,
+                                  spreadRadius: 0.5,
+                                  blurRadius: 5,
+                                  offset: Offset(3, 6),
+                                ),
+                              ],
                             ),
                           ),
                         ),
